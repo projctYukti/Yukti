6821db348ff1027e1578421d118354654bd51b90
fahim <fahimislam7860@gmail.com>
2024-11-26 23:15:36 +0530

    Businesses added

--- app/src/main/java/com/example/yukti/navigation/Routes.kt
+++ app/src/main/java/com/example/yukti/navigation/Routes.kt
@@ -4,4 +4,6 @@ object Routes {
     var signIn = "sign_in"
     var chat = "chat"
     var subscriptionPage = "subscription_page"
+    const val businessSetup = "businessSetup"
+    const val joinBusiness = "joinBusiness"
 }
