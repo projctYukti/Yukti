06c7a89cb4f87efcc7005557c6ce588867f3d45b
fahim <fahimislam7860@gmail.com>
2026-01-18 19:06:23 +0530

    Package name changed

--- app/src/main/java/com/example/yukti/chat/MessageModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.yukti.chat
-
-data class MessageModel(
-    val message : String,
-    val role : String,
-    val timestamp : String
-
-    )
-
--- app/src/main/java/com/example/yukti/sign_in/SignInState.kt
+++
@@ -1,6 +0,0 @@
-package com.example.yukti.sign_in
-
-data class SignInState(
-    val isSignInSuccessful: Boolean = false,
-    val signInError: String? = null
-)
